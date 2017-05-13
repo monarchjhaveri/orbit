6aaca0d0df0946db81665b1ea02ba77131dc542d
Monarch Wadia <monarchwadia@gmail.com>
2017-05-12 20:05:56 -0400

    Added some comments, cleaned up some code.

--- client/src/events/IncrementEvent.js
+++ client/src/events/IncrementEvent.js
@@ -1,8 +1,4 @@
 export default class IncrementEvent {
-  // constructor(...payload) {
-  //   this.payload = payload;
-  // }
-
   process(state) {
     const val = state.val;
 
--- client/src/orbit/index.js
+++ client/src/orbit/index.js
@@ -13,29 +13,38 @@ function reactRender(Component, props, element) {
 }
 
 class Application {
+  // You initialize the application by passing in an initial state.
   constructor(initialState) {
     this._state = initialState;
+
+    // the root component type
     this._rootComponent = null;
+
+    // the target element
     this._element = null;
   }
 
+  // used to get the state. don't ever get _state by itself.
   getState() {
     return this._state;
   }
 
+  // use this instead of ReactDOM.render
   render(Component, element) {
     this._rootComponent = Component;
     this._element = element;
     reactRender(this._rootComponent, {app: this}, this._element);
   }
 
-  updateListeners() {
-    reactRender(this._rootComponent, {app: this}, this._element);
+  // change state using this
+  trigger(eventInstance) {
+    this._state = update(this._state, eventInstance.process(this._state));
+    this.updateListeners();
   }
 
-  trigger(event) {
-    this._state = update(this._state, event.process(this._state));
-    this.updateListeners();
+  // private methodss
+  _updateListeners() {
+    reactRender(this._rootComponent, {app: this}, this._element);
   }
 }
 
